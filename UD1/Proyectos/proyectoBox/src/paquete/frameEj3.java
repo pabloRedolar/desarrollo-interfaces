8e74be63fdad24674b59ce1effcffc2a9d7311d3
pabloRedolar <predolars@iesch.org>
2024-10-09 19:34:01 +0200

    Ejercicios spinners

--- UD1/Proyectos/proyectoBox/src/paquete/frameEj3.java
+++ UD1/Proyectos/proyectoBox/src/paquete/frameEj3.java
@@ -32,11 +32,11 @@ public class frameEj3 extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 484, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 422, Short.MAX_VALUE)
         );
 
         pack();
