55effbb12aec9c780ed0d4d32f057c4279261e42
pabloRedolar <predolars@iesch.org>
2025-02-10 19:33:19 +0100

    a

---
+++ UD4/practicaInformes/src/main/java/org/iesch/di/ud4conectordb/Launcher.java
@@ -0,0 +1,9 @@
+package org.iesch.di.ud4conectordb;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
